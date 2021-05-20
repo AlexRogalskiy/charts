623956da0c4a4de7c9a17c0cea8aa15400d4f077
ADMSK\AVROGAL1 <avrogal1@mts.ru>
2021-05-20 17:07:21 +0300

    docs: updates on workflows
    Added information on documentation

--- scripts/install.js
+++ scripts/install.js
@@ -1,16 +1,42 @@
 const util = require('util');
+
 const exec = util.promisify(require('child_process').exec);
+const execFile = util.promisify(require('child_process').execFile);
+
+const isWindowsOS = process.platform === 'win32'
 
 async function installDependencies() {
-    const { stdout, stderr } = await exec('apt-get install libuuid1');
-    console.log('stdout:', stdout);
-    console.error('stderr:', stderr);
-}
-
-async function runCommand() {
-    if (process.platform !== 'win32') {
-        await installDependencies()
+    if (!isWindowsOS) {
+        const { stdout, stderr } = await exec('apt-get install libuuid1');
+        console.log('dependencies logs:', stdout);
+        console.error('dependencies errors:', stderr);
     }
 }
 
-runCommand();
+async function getDirectoryList() {
+    if (!isWindowsOS) {
+        const { stdout } = await exec('ls', ['-lh']);
+        console.log('directory list:', stdout);
+    }
+}
+
+async function getProcessList() {
+    if (!isWindowsOS) {
+        const { stdout } = await exec('ps', ['ax']);
+        console.log('process list:', stdout);
+    }
+}
+
+async function getNodeVersion() {
+    const { stdout } = await execFile('node', ['--version']);
+    console.log('node version:', stdout);
+}
+
+async function runCommands() {
+    await getNodeVersion();
+    await getProcessList();
+    await getDirectoryList();
+    await installDependencies();
+}
+
+runCommands();
