f60b50f19fc43d1c1caf90f5f9b2588f2bfd05f6
AlexRogalskiy <a.rogalskii@bftcom.com>
2021-08-09 12:31:07 +0300

    Added info on workflows
    Updates on github-actions

---
+++ docker_launch.sh
@@ -0,0 +1,9 @@
+#!/bin/sh
+
+$DOCKER_SUBNET="10.0.${SUBNET}.0/24"
+
+if [ -z "$SUBNET" ]; then
+  docker-compose -f docker-compose.yml -f docker-compose-net-auto.yml up
+else
+  docker-compose -f docker-compose.yml -f docker-compose-net-subnet.yml up
+fi
